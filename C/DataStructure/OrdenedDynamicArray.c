f4964ba8ee4f8aa0c1a9176782fd4dc2dc561a93
Guilherme.zanan <mnbvcasadg@gmail.com>
2024-05-04 16:32:02 -0400

    Funcao de remover e desalocar feitas de testadas

--- C/DataStructure/OrdenedDynamicArray.c
+++ C/DataStructure/OrdenedDynamicArray.c
@@ -8,10 +8,8 @@
 #include <stdlib.h>
 
 typedef struct array{
-
 	int *end;
 	int size;
-    
 }array;
  
 void arrayInnit(array *array){
@@ -35,44 +33,34 @@ void arrayInsert(array *array, int x){
 		if(array->end[i]>=x){
 			for(j=array->size-1;j>=i;j--){
 				array->end[j+1] = array->end[j];
-			
 			}
 			array->end[i] = x;
 			i = array->size;	
-
 		}
-		
-
   	}
 }
 
-void arrayRemove(array array, int x){
+void arrayRemove(array *array, int x){
 	unsigned i,j;
 
-	for(i=0;i<=array.size;i++){
-		if(array.end[i]==x){
-			array.end[i]=0;
-			for(j=i+1;j<=array.size;i++){
-				array.end[j] = array.end[j-1];
+	for(i=0;i<=array->size-1;i++){
+
+		if(array->end[i]==x){
+
+			for(j=i+1;j<=array->size-1;j++){
+				array->end[j-1] = array->end[j];
 			}
-			array.size--;
-			array.end = realloc(array.end, sizeof(int) * array.size);
+			array->size--;
+			array->end = realloc(array->end, sizeof(int) * array->size);
 		}
 	}
 }
 
-int main(){
-	array a1;
-	arrayInnit(&a1);
-	arrayInsert(&a1,6);
-	arrayInsert(&a1,4);
-	arrayInsert(&a1,5);
-	for(unsigned i=0; i<=a1.size-1;i++){
-		printf("%d ",a1.end[i]);
-	}
-	//arrayRemove(a1,5);
-	return 0;
-
-
+void arrayDeallocate(array *array){
+	printf("%p ",array);
+	free(array);
+	printf("%p",array);
 }
+
+
 
