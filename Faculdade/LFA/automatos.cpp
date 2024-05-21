6129e0cd05440d2fd8e01283ff343e44e8151036
FrostByte <84871179+Frostbite16@users.noreply.github.com>
2024-05-21 17:24:16 +0000

    Setters

--- Faculdade/LFA/automatos.cpp
+++ Faculdade/LFA/automatos.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+#include <string>
+
+using namespace std;
 
 class automato{
-    char Q[10], F[10], q, a;
-    char Q[10][10];
+    char Q[10], F[10], A[10], q, a;
+    string P[30]; 
+    char Ma[10][10];
+    
+    public:
+        
+        void setQ(char c, int i){
+            Q[i] = c;
+        }
+
+        void setF(char c, int i){
+            F[i] = c;
+        }
+
+        void setA(char c, int i){
+            A[i] = c;
+        }
+    
+
+
+
+
+
+
+
+    public:
+
 
 };
