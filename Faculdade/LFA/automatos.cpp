17d95eaf78c750b485eec1970d02a5b91e4480ab
Guilherme.zanan <mnbvcasadg@gmail.com>
2024-05-22 11:25:59 -0400

    NOVO COMMIT

--- Faculdade/LFA/automatos.cpp
+++ Faculdade/LFA/automatos.cpp
@@ -1,32 +1,54 @@
 
 #include <iostream> /* cin, cout */
 #include <string> /* string */
+#define size 10
 
 using namespace std;
 
 class automato{
-    char estados[10], estadosFinais[10], alfabeto[10], est_Inicial;
+    char estados[size], estadosFinais[size], alfabeto[size], est_Inicial;
     string PalavraAtual[30], palavraAceita[30]; 
-    char matrizAlpha[10][10];
-    int alphaSize, clock;
+    char matrizAlpha[size][size];
+    int estadoSize=0, alfaSize=0;
     
     public:
         
-        void addEstados(char c, int i){ // Adiciona novos estados com nome c na posicao i do vetor estados[]
-
+        bool addEstados(char c){ // Adiciona novos estados com nome c na posicao i do vetor estados[] caso o estado não exista
+            for(int i=0; i<estadoSize; ++i){
+                if(c==estados[i]){
+                    cout << "Estado já existe";
+                    return false;
+                }
+            }
+            estadoSize+=1;
+            estados[estadoSize-1]=c;
+            return true;
         }
 
         void addEstadosFinais(char c, int i){ // Adiciona novos estados finais com nome c na posicao i do vetor estadosFinais[]
-            estadosFinais[i] = c;
+            if(checkPar(c,estados)){
+                estadosFinais[i] = c;
+            }
+            else{
+                cout << "Estado não encontrado";
+            }
         }
 
-        void addAlfabeto(char c, int i){ // Adiciona novos simbolos c na posicao i do vetor alfabeto[]
-            alfabeto[i] = c;
+        bool addAlfabeto(char c, int i){ // Adiciona novos simbolos c na posicao i do vetor alfabeto[]
+            for(int i=0; i<alfaSize; ++i){
+                if(c==alfabeto[i]){
+                    cout << "Simbolo já está dentro do alfabeto";
+                    return false;
+                }
+            }
+            alfaSize++;
+            estados[alfaSize-1]=c;
+            return true;
         }
         
-        bool checkAlfa(char c){ // Checa se um simbolo já está na no vetor alfabeto[], caso esteja retorna falso
-            for(int i=0;i<alphaSize;++i){
-                if(c==alfabeto[i]){
+        bool checkPar(char c, char *Par){ // Checa se um simbolo já está na em um vetor Par, caso esteja retorna falso
+            for(int i=0;i<size;++i){
+                if(c==Par[i]){
                     return true;
                 }
             }
