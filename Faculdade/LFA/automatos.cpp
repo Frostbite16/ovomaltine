b2be6f80e88ce4bca0db900d4b5ea3c4ff931e5c
Guilherme.zanan <mnbvcasadg@gmail.com>
2024-05-23 01:22:22 -0400

    AAAAAAAAAAAAAAAAAAAAAAA a

--- Faculdade/LFA/automatos.cpp
+++ Faculdade/LFA/automatos.cpp
@@ -1,53 +1,109 @@
 
 #include <iostream> /* cin, cout */
 #include <string> /* string */
-#define size 10
+#include <cstdio> /* getchar() */
+
 
 using namespace std;
 
 class automato{
-    char estados[size], estadosFinais[size], alfabeto[size], est_Inicial;
-    string PalavraAtual[30], palavraAceita[30]; 
-    char matrizAlpha[size][size];
-    int estadoSize=0, alfaSize=0;
+    string estados, estadosFinais, alfabeto;
+    string PalavraAtual, palavraAceita; 
+    char matrizAlpha[10][10],estInicial;
+    int estadoSize=0, alfaSize=0, clock=0;
+    int i,j,k;
     
     public:
         
-        bool addEstados(char c){ // Adiciona novos estados com nome c na posicao i do vetor estados[] caso o estado não exista
-            for(int i=0; i<estadoSize; ++i){
-                if(c==estados[i]){
-                    cout << "Estado já existe";
-                    return false;
+        automato()
+        {}
+        
+        void addEstados(){ // Adiciona novos estados com nome c na posicao i do vetor estados[] caso o estado não exista
+            getline(cin, estados);
+
+            for(i=0;i<estados.size();i++){
+                if(estados[i]==','){
+
+                    clock++;
+                    for(j=i;j<estados.size();j++){
+                        estados[j]=estados[j+1];
+
+                    }
                 }
             }
-            estadoSize+=1;
-            estados[estadoSize-1]=c;
-            return true;
+
+            estados.resize(estados.size()-clock);
+            clock=0;
+        }
+        
+        bool CheckEstados(char c){
+            for(i=0;i<estados.size();i++){
+                if(c==estados[i])
+                    return true;
+            }
+            return false;
+        }
+        
+        bool setEstInicial(){
+            char c = getchar();
+            if(CheckEstados(c)){
+                estInicial = c;
+                return true;
+            }
+            return false;
+            
         }
 
-        void addEstadosFinais(char c, int i){ // Adiciona novos estados finais com nome c na posicao i do vetor estadosFinais[]
-            if(checkPar(c,estados)){
-                estadosFinais[i] = c;
-            }
-            else{
-                cout << "Estado não encontrado";
-            }
-        }
+        void addEstadosFinais(){ // Adiciona novos estados finais com nome c na posicao i do vetor estadosFinais[]
+            getline(cin, estadosFinais);
 
-        bool addAlfabeto(char c, int i){ // Adiciona novos simbolos c na posicao i do vetor alfabeto[]
-            for(int i=0; i<alfaSize; ++i){
-                if(c==alfabeto[i]){
-                    cout << "Simbolo já está dentro do alfabeto";
-                    return false;
+            for(i=0;i<estadosFinais.size();i++){
+
+                if(estadosFinais[i]==','){
+
+                    clock++;
+                    for(j=i;j<estadosFinais.size();j++){
+                        estadosFinais[j]=estadosFinais[j+1];
+                    }
                 }
             }
-            alfaSize++;
-            estados[alfaSize-1]=c;
-            return true;
+            estadosFinais.resize(estadosFinais.size()-clock);
+            clock=0;
+            for(i=0;i<estadosFinais.size();i++){
+                if(!(CheckEstados(estadosFinais[i]))){
+                    clock++;
+                    if(!(i==estadosFinais.size()-1)){
+                        for(j=i;j<estadosFinais.size()-1;j++){
+                            estadosFinais[j]=estadosFinais[j+1];
+                        }
+                    }
+                    estadosFinais.resize(estadosFinais.size()-clock);
+                    clock=0;
+     
+                }
+            }
+            
+        }
+
+        void addAlfabeto(){ // Adiciona novos simbolos c na posicao i do vetor alfabeto[]
+            getline(cin, alfabeto);
+            for(i=0;i<alfabeto.size();i++){
+
+                if(alfabeto[i]==','){
+
+                    clock++;
+                    for(j=i;j<alfabeto.size();j++){
+
+                        alfabeto[j]=alfabeto[j+1];
+                    }
+                }
+            }
+            alfabeto.resize(alfabeto.size()-clock);
+            clock=0;
         }
         
         bool checkPar(char c, char *Par){ // Checa se um simbolo já está na em um vetor Par, caso esteja retorna falso
-            for(int i=0;i<size;++i){
+            for(int i=0;i<10;++i){
                 if(c==Par[i]){
                     return true;
                 }
@@ -55,20 +111,23 @@ class automato{
             return false;
         }
 
-        void funcTransicao(unsigned i, unsigned j, unsigned c){ // adiciona novas relacoes entre os estados
-            
+        void funcTransicao(){ // adiciona novas relacoes entre os estados
+            for(i=0;i<estados.size();i++){
+                for(j=0;j<alfabeto.size();j++){
+                    printf("Transicao(%c, %c)\n",estados[i],alfabeto[j]);
+                    scanf("%c",&matrizAlpha[i][j]);
+                }
+            }
 
         }
 
-    
-
-
-
-
-
-
-
-    public:
-
 
 };
+
+int main(){
+    automato a1;
+    a1.addAlfabeto();
+    a1.addEstados();
+    a1.addEstadosFinais();
+    a1.funcTransicao();
+}
