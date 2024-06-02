1392b19316249c09b562423fe368e87466f950c2
Guilherme.zanan <mnbvcasadg@gmail.com>
2024-06-02 19:05:57 -0400

    A

--- Faculdade/AED-II/T2/main.c
+++ Faculdade/AED-II/T2/main.c
@@ -1,4 +1,4 @@
-#include "gfx/gfx.h"
+//#include "\gfx\gfx.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,52 +7,82 @@ typedef struct listaSim{ //estrutura de uma lista simplismente encadeada
 	struct listaSim* prox;
 }listaSim;
 
+typedef struct listaSimPilha{ //estrutura de uma lista simplismente encadeada
+	struct listaSimPilha* LastNode;
+}Pilha;
+
+typedef struct listaSimFila{ //estrutura de uma lista simplismente encadeada
+	struct listaSimFila *FirstNode, *LastNode;
+}Fila;
+
 typedef struct listaDup{ //estrutura de uma lista duplamente encadeada
 	int data;
 	struct listaDup *prox, *ant;
 }listaDup;
 
 
-void insertListSimp(listaSim* h, int t, int esc){ // Aloca uma lista simplismente encadeada, com ou sem no cabeca;
-	int i, j=1;
-	if(esc==0){
-		listaSim* c = h;
-		printf("Elemento do %d° no da lista:\n",j);
-		scanf("%d",&c->data);
-		j++;
-	else{
-		listaSim* head = malloc(sizeof(int));
-		listaSim* c = head;
-		h = head;
-	}
-	
 
-	for(i=0;i<t;i++){
-	
-		listaSim* p = malloc(sizeof(int));
-		printf("Elemento do %d° no da lista:\n",j);
-		scanf("%d",&p->data);
-		j++;
-		c->prox = p;
-		c = p;	
+int AddlistSimOrd(listaSim* h, int x){ // Insercao de uma lista simplismente encadeada e ordenada sem no cabeca
+	listaSim *c = malloc(sizeof(listaSim)), *p=h;
+	c->data = x;
+	if(h=NULL){
+		h=c;
+		c->prox = NULL;
+		return 1;
 	}
-
-	c->next = NULL;
+	while((p->prox->data<=x)&&(p->prox!=NULL)){
+		p = p->prox;
+	}
+	c->prox = p->prox;
+	p->prox = c;
+	return 1;
 }
 
-void insertListDup(listaDup* head, int t){
+listaSim* buscaListSimOrd(listaSim* h, int x){
+	listaSim *p = h;
+	if(h=NULL){
+		return NULL;
+	}
+	while((p->data<x)||(p->prox!=NULL)){
+		if(p->data==x){
+			return p;
+		}
+		p = p->prox; 
+	}
+	if(p->data!=x){
+		return NULL;
+	}
+	return p;
+}
+
+int removeListSimOrd(listaSim* h, int x){
+	
+
+
+
+}
+
+int insertListDup(listaDup* head, int x){ // Insercao de uma lista duplamente encadeada com no cabeca
 
 	int i;
-	listaDup* c = h;
-	
-	for(i=0;i<t;i++){
-	
-	
+	listaDup* c = malloc(sizeof(listaDup)), *p=head;
+	c->data = x;
+	if(head==NULL){
+		head = malloc(sizeof(listaDup));
+		head->prox = c;
+		head->ant = NULL;
+		c->prox = NULL;
+		c->ant = head;
+		return 1;
 	}
 	
-
-
-
+	while(p->prox!=NULL){
+		p = p->prox;
+	}
+	p->prox=c;
+	c->prox = NULL;
+	c->ant = p;
+	return 1;
 
 }
 
