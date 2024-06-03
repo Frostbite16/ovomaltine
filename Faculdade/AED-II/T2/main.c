7c3afeca6e92d3ffb866fa2af7473e7b6626f386
FrostByte <84871179+Frostbite16@users.noreply.github.com>
2024-06-03 22:51:19 +0000

    Added remove function

--- Faculdade/AED-II/T2/main.c
+++ Faculdade/AED-II/T2/main.c
@@ -1,4 +1,4 @@
-//#include "\gfx\gfx.h"
+#include "gfx/gfx.h"
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -25,7 +25,7 @@ typedef struct listaDup{ //estrutura de uma lista duplamente encadeada
 int AddlistSimOrd(listaSim* h, int x){ // Insercao de uma lista simplismente encadeada e ordenada sem no cabeca
 	listaSim *c = malloc(sizeof(listaSim)), *p=h;
 	c->data = x;
-	if(h=NULL){
+	if(h==NULL){
 		h=c;
 		c->prox = NULL;
 		return 1;
@@ -40,7 +40,7 @@ int AddlistSimOrd(listaSim* h, int x){ // Insercao de uma lista simplismente enc
 
 listaSim* buscaListSimOrd(listaSim* h, int x){
 	listaSim *p = h;
-	if(h=NULL){
+	if(h==NULL){
 		return NULL;
 	}
 	while((p->data<x)||(p->prox!=NULL)){
@@ -56,10 +56,24 @@ listaSim* buscaListSimOrd(listaSim* h, int x){
 }
 
 int removeListSimOrd(listaSim* h, int x){
-	
-
-
-
+	listaSim *c = NULL, *p = h;
+	if(h!=NULL){
+		c = p;
+		while((p->prox!=NULL)&&(p->data<=x)){
+			c = p;
+			p=p->prox;
+		}
+		if(p->data==x){
+			c->prox = p->prox;
+			if(p==h){
+				h=p->prox;
+			}
+			free(p);
+			return 1;
+		}
+		return 0;
+	}
+	return 0;
 }
 
 int insertListDup(listaDup* head, int x){ // Insercao de uma lista duplamente encadeada com no cabeca
@@ -83,7 +97,14 @@ int insertListDup(listaDup* head, int x){ // Insercao de uma lista duplamente en
 	c->prox = NULL;
 	c->ant = p;
 	return 1;
+}
 
+listaDup* buscaListDup(listaDup* head, int x){
+	listaDup* c = NULL;
+	if(head!=NULL){
+		while(c->prox!=NULL){
+		}
+	}
 }
 
 
