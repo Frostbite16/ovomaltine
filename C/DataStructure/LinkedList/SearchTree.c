1ec2eaa2a40c509b2c47621255e65affc17abd7d
FrostByte <84871179+Frostbite16@users.noreply.github.com>
2024-06-15 18:47:11 +0000

    Added search tree

--- C/DataStructure/LinkedList/SearchTree.c
+++ C/DataStructure/LinkedList/SearchTree.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct sTree{
+    int data:32;
+    int *proxL;
+    int *proxR;
+}sTreeNode;
+
+sTreeNode* insertSTree(sTreeNode* root, int x){
+    sTreeNode* c = malloc(sizeof(sTreeNode)), *p=root;
+
+    if(root==NULL){
+        c->data=x;
+        c->proxL=NULL;
+        c->proxR=NULL;
+        return c;
+    }
+    while(p->data>x){
+       
+    }
+
+}
+
 
