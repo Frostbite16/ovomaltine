3e91c7e179147e60f80b6023d8f802305c4df990
guilherme <mnbvcasadg@gmail.com>
2024-06-26 15:56:56 -0400

    pasta do projeto de extensao

---
+++ Faculdade/PP/Metrics.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+class meters{
+	float size;
+	float operator(){}{
+		
+	}
+
+};
+class miles;
+
+class meters{
+	float size;
+	public:
+		meters(float size) size(0)
+		{
+			this->size=size
+		}
+		meters(const miles miles){
+
+		}
+	
+};
+
+
+
