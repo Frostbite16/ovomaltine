a70188c23a5c70611bcb27d08c2d3068123b50b0
guilherme <mnbvcasadg@gmail.com>
2024-09-12 09:46:00 -0400

    Trabalho 5 - inicio

---
+++ Faculdade/AED-II/T5/main.c
@@ -0,0 +1,19 @@
+#ifdef __linux__
+	#define clear "clear"
+#elif WIN_32
+	#define clear "cls"
+#endif
+
+
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct num_file{
+	int32_t num;
+}num_file;
+
+
+
+int main(){
+
+}
